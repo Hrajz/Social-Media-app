4c727fdfbdf20c046b64e695f9639ac7314c1633
hrajz <himansurajz22@gmail.com>
2024-05-04 14:25:50 +0530

    completed users model

--- backend/Models/users.model.js
+++ backend/Models/users.model.js
@@ -4,14 +4,97 @@ import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    userid: {
+    firstName: {
+      type: String,
+      required: true,
+      // unique: true,
+      // lowercase: true,
+      // trim: true,
+      // index: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true,
-      index: true,
     },
+    dp: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+    },
+    homeTown: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
+    work: {
+      type: String,
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    friendRequest: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    story: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "story",
+      },
+    ],
+    notification: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "notification",
+      },
+    ],
+    acrivity: [
+      {
+        type: String,
+      },
+    ],
+    profilePictures: [
+      {
+        type: String,
+      },
+    ],
+    coverImages: [
+      {
+        type: String,
+      },
+    ],
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    messages: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+        message: [
+          {
+            type: String,
+          },
+        ],
+      },
+    ],
   },
   {
     timestamps: true,
